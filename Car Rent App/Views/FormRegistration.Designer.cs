3bf60f3ae8f79bb0eba2f25fc0e44ba51cef15b8
Emir <tashievemir01@gmail.com>
2023-05-07 16:19:24 +0600

    add registration page

---
+++ Car Rent App/Views/FormRegistration.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Car_Rent_App.Views
+{
+    partial class FormRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelRegistr = new System.Windows.Forms.Panel();
+            this.lb_registr = new System.Windows.Forms.Label();
+            this.lb_name = new System.Windows.Forms.Label();
+            this.lb_surname = new System.Windows.Forms.Label();
+            this.lb_phone = new System.Windows.Forms.Label();
+            this.lb_passw = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lb_passwRepeat = new System.Windows.Forms.Label();
+            this.btn_registr = new System.Windows.Forms.Button();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.tb_surname = new System.Windows.Forms.TextBox();
+            this.tb_phone = new System.Windows.Forms.TextBox();
+            this.tb_emailRegistr = new System.Windows.Forms.TextBox();
+            this.tb_passw = new System.Windows.Forms.TextBox();
+            this.tb_passwRepeat = new System.Windows.Forms.TextBox();
+            this.panelRegistr.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelRegistr
+            // 
+            this.panelRegistr.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            this.panelRegistr.Controls.Add(this.tb_passwRepeat);
+            this.panelRegistr.Controls.Add(this.tb_passw);
+            this.panelRegistr.Controls.Add(this.tb_emailRegistr);
+            this.panelRegistr.Controls.Add(this.tb_phone);
+            this.panelRegistr.Controls.Add(this.tb_surname);
+            this.panelRegistr.Controls.Add(this.tb_name);
+            this.panelRegistr.Controls.Add(this.btn_registr);
+            this.panelRegistr.Controls.Add(this.lb_passwRepeat);
+            this.panelRegistr.Controls.Add(this.label2);
+            this.panelRegistr.Controls.Add(this.lb_passw);
+            this.panelRegistr.Controls.Add(this.lb_phone);
+            this.panelRegistr.Controls.Add(this.lb_surname);
+            this.panelRegistr.Controls.Add(this.lb_name);
+            this.panelRegistr.Controls.Add(this.lb_registr);
+            this.panelRegistr.Location = new System.Drawing.Point(50, 40);
+            this.panelRegistr.Name = "panelRegistr";
+            this.panelRegistr.Size = new System.Drawing.Size(500, 500);
+            this.panelRegistr.TabIndex = 0;
+            // 
+            // lb_registr
+            // 
+            this.lb_registr.AutoSize = true;
+            this.lb_registr.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.lb_registr.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(53)))), ((int)(((byte)(101)))));
+            this.lb_registr.Location = new System.Drawing.Point(158, 41);
+            this.lb_registr.Name = "lb_registr";
+            this.lb_registr.Size = new System.Drawing.Size(174, 38);
+            this.lb_registr.TabIndex = 0;
+            this.lb_registr.Text = "Registration";
+            // 
+            // lb_name
+            // 
+            this.lb_name.AutoSize = true;
+            this.lb_name.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.lb_name.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(53)))), ((int)(((byte)(101)))));
+            this.lb_name.Location = new System.Drawing.Point(76, 113);
+            this.lb_name.Name = "lb_name";
+            this.lb_name.Size = new System.Drawing.Size(79, 29);
+            this.lb_name.TabIndex = 1;
+            this.lb_name.Text = "Name:";
+            // 
+            // lb_surname
+            // 
+            this.lb_surname.AutoSize = true;
+            this.lb_surname.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.lb_surname.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(53)))), ((int)(((byte)(101)))));
+            this.lb_surname.Location = new System.Drawing.Point(76, 160);
+            this.lb_surname.Name = "lb_surname";
+            this.lb_surname.Size = new System.Drawing.Size(109, 29);
+            this.lb_surname.TabIndex = 2;
+            this.lb_surname.Text = "Surname:";
+            // 
+            // lb_phone
+            // 
+            this.lb_phone.AutoSize = true;
+            this.lb_phone.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.lb_phone.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(53)))), ((int)(((byte)(101)))));
+            this.lb_phone.Location = new System.Drawing.Point(76, 212);
+            this.lb_phone.Name = "lb_phone";
+            this.lb_phone.Size = new System.Drawing.Size(79, 29);
+            this.lb_phone.TabIndex = 3;
+            this.lb_phone.Text = "Phone:";
+            // 
+            // lb_passw
+            // 
+            this.lb_passw.AutoSize = true;
+            this.lb_passw.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.lb_passw.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(53)))), ((int)(((byte)(101)))));
+            this.lb_passw.Location = new System.Drawing.Point(80, 314);
+            this.lb_passw.Name = "lb_passw";
+            this.lb_passw.Size = new System.Drawing.Size(112, 29);
+            this.lb_passw.TabIndex = 4;
+            this.lb_passw.Text = "Password:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(53)))), ((int)(((byte)(101)))));
+            this.label2.Location = new System.Drawing.Point(80, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Email:";
+            // 
+            // lb_passwRepeat
+            // 
+            this.lb_passwRepeat.AutoSize = true;
+            this.lb_passwRepeat.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.lb_passwRepeat.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(53)))), ((int)(((byte)(101)))));
+            this.lb_passwRepeat.Location = new System.Drawing.Point(80, 362);
+            this.lb_passwRepeat.Name = "lb_passwRepeat";
+            this.lb_passwRepeat.Size = new System.Drawing.Size(191, 29);
+            this.lb_passwRepeat.TabIndex = 6;
+            this.lb_passwRepeat.Text = "Password Repeat:";
+            // 
+            // btn_registr
+            // 
+            this.btn_registr.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(45)))), ((int)(((byte)(112)))));
+            this.btn_registr.ForeColor = System.Drawing.Color.White;
+            this.btn_registr.Location = new System.Drawing.Point(186, 434);
+            this.btn_registr.Name = "btn_registr";
+            this.btn_registr.Size = new System.Drawing.Size(118, 40);
+            this.btn_registr.TabIndex = 7;
+            this.btn_registr.Text = "Registrate";
+            this.btn_registr.UseVisualStyleBackColor = false;
+            // 
+            // tb_name
+            // 
+            this.tb_name.Location = new System.Drawing.Point(154, 115);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(150, 30);
+            this.tb_name.TabIndex = 8;
+            // 
+            // tb_surname
+            // 
+            this.tb_surname.Location = new System.Drawing.Point(182, 162);
+            this.tb_surname.Name = "tb_surname";
+            this.tb_surname.Size = new System.Drawing.Size(150, 30);
+            this.tb_surname.TabIndex = 9;
+            // 
+            // tb_phone
+            // 
+            this.tb_phone.Location = new System.Drawing.Point(154, 214);
+            this.tb_phone.Name = "tb_phone";
+            this.tb_phone.Size = new System.Drawing.Size(150, 30);
+            this.tb_phone.TabIndex = 10;
+            // 
+            // tb_emailRegistr
+            // 
+            this.tb_emailRegistr.Location = new System.Drawing.Point(154, 265);
+            this.tb_emailRegistr.Name = "tb_emailRegistr";
+            this.tb_emailRegistr.Size = new System.Drawing.Size(150, 30);
+            this.tb_emailRegistr.TabIndex = 11;
+            // 
+            // tb_passw
+            // 
+            this.tb_passw.Location = new System.Drawing.Point(186, 316);
+            this.tb_passw.Name = "tb_passw";
+            this.tb_passw.Size = new System.Drawing.Size(150, 30);
+            this.tb_passw.TabIndex = 12;
+            this.tb_passw.UseSystemPasswordChar = true;
+            // 
+            // tb_passwRepeat
+            // 
+            this.tb_passwRepeat.Location = new System.Drawing.Point(268, 364);
+            this.tb_passwRepeat.Name = "tb_passwRepeat";
+            this.tb_passwRepeat.Size = new System.Drawing.Size(150, 30);
+            this.tb_passwRepeat.TabIndex = 13;
+            this.tb_passwRepeat.UseSystemPasswordChar = true;
+            // 
+            // FormRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.panelRegistr);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FormRegistration";
+            this.Text = "FormRegistration";
+            this.panelRegistr.ResumeLayout(false);
+            this.panelRegistr.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panelRegistr;
+        private Button btn_registr;
+        private Label lb_passwRepeat;
+        private Label label2;
+        private Label lb_passw;
+        private Label lb_phone;
+        private Label lb_surname;
+        private Label lb_name;
+        private Label lb_registr;
+        private TextBox tb_passwRepeat;
+        private TextBox tb_passw;
+        private TextBox tb_emailRegistr;
+        private TextBox tb_phone;
+        private TextBox tb_surname;
+        private TextBox tb_name;
+    }
+}
