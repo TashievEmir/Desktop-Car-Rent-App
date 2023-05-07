550a3490fe5918de1b41036d1cf8543544768af0
Emir <tashievemir01@gmail.com>
2023-05-07 15:48:47 +0600

    Auth page done

---
+++ Car Rent App/Views/FormAuth.cs
@@ -0,0 +1,15 @@
+namespace Car_Rent_App
+{
+    public partial class FormAuth : Form
+    {
+        public FormAuth()
+        {
+            InitializeComponent();
+        }
+
+        private void tb_email_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
